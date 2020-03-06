02ca3a9ae41a44a881f10747333688ea06444d0b
Ryan Homer <ryan.homer@alumni.ubc.ca>
2020-03-06 00:24:53 -0800

    Rename filename using snake_case

--- feature_selection/__init__.py
+++ feature_selection/__init__.py
@@ -1,2 +1,2 @@
 __version__ = '0.1.0'
-from feature_selection.RecursiveFeatureElimination import recursive_feature_elimination
+from feature_selection.recursive_feature_elimination import recursive_feature_elimination
