e53e480cab74231e55925b82e7f16d2aa73cceae
Victor Cuspinera <vcuspinera@gmail.com>
2020-03-13 16:47:09 -0700

    Update example importing forward function

--- feature_selection/forward_selection.py
+++ feature_selection/forward_selection.py
@@ -34,6 +34,7 @@ def forward_selection(scorer, X, y, min_features=1, max_features=10):
     >>> from sklearn.linear_model import LinearRegression
     >>> from sklearn.datasets import make_friedman1
     >>> data, target = make_friedman1(n_samples=200, n_features=15, random_state=0)
+    >>> from feature_selection import forward_selection
     >>>
     >>> def my_scorer_fn2(X, y):
     >>>      lm = LinearRegression().fit(X, y)
