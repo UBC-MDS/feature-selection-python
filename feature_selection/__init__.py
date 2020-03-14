81e3810c3994629deea7e1de0d33f203739a5bc3
semantic-release <semantic-release>
2020-03-14 22:41:22 +0000

    1.1.2
    Automatically generated by python-semantic-release

--- feature_selection/__init__.py
+++ feature_selection/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 from feature_selection.forward_selection import forward_selection
 from feature_selection.simulated_annealing import simulated_annealing
